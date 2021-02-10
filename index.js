c714ae249598d75aac924f781afc6e77ff57f6dd
Leao-E <eemanuelleao@gmail.com>
2021-02-09 23:59:08 -0300

    Additions to the JS script: AppDom to render the table tbody, Transaction to handle new transactions, transactions to store the transactions in memory

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const _$ = query => document.querySelector(query);
+
 const Modal = {
-   modalOverlay: document.querySelector('.modal-overlay'),
+   modalOverlay: _$('.modal-overlay'),
    toogle(event){
       event.preventDefault();
       this.modalOverlay.classList.toggle('active');
@@ -7,5 +9,77 @@ const Modal = {
 }
 
 const Transaction = {
+   incomes () {
 
+   },
+   expenses () {
+
+   },
+   total () {
+
+   }
 }
+
+const transactions = [
+   {
+      id: 1,
+      amount: 500_00,
+      date: '27/02/2021',
+      description: 'LaLALAn'
+   },
+   {
+      id: 2,
+      amount: -4500_59,
+      date: '27/02/2021',
+      description: 'Sei lá!'
+   },
+   {
+      id: 2,
+      amount: 750_00,
+      date: '27/02/2021',
+      description: 'LaLALAn'
+   },
+];
+
+const AppDOM  = {
+   transactionsContainer: _$('#data-table tbody'),
+   addTransaction(transaction, index){
+      const tr = document.createElement('tr');
+      tr.innerHTML = AppDOM.transactionInnerHTML(transaction);
+
+      AppDOM.transactionsContainer.appendChild(tr);
+   },
+   transactionInnerHTML(transaction){
+      const positive = transaction.amount > 0;
+      const cssClass = positive ? 'income' : 'expanse';
+      const amount = Utils.formatCurrency(transaction.amount);
+
+      return `
+      <td class="description">${transaction.description}</td>
+      <td class="${cssClass}">${amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+         <img src="./assets/minus.svg" alt="Remover transação">
+      </td>
+      `;
+   }
+}
+
+const Utils = {
+   formatCurrency(value){
+      const signal = Number(value) < 0 ? '-' : '';
+
+      value = String(value).replace(/\D/g, '');
+
+      value = Number(value) / 100;
+
+      value = value.toLocaleString('pt-BR', {
+         style: 'currency',
+         currency: 'BRL'
+      });
+
+      return signal + value;
+   }
+}
+
+transactions.forEach(transaction => AppDOM.addTransaction(transaction));
